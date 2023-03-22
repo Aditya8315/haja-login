bcd59cfdaf5b26d6a94608027c8a193594f19c19
Aditya Priyanshu <106824520+Aditya8315@users.noreply.github.com>
2023-03-22 14:41:49 +0530

    Add files via upload

---
+++ Login-main/pages/index.js
@@ -0,0 +1,46 @@
+import styles from '../styles/Home.module.css'
+import Router, { useRouter } from 'next/router'
+export default function Home() {
+  const router = useRouter()
+  return (
+    <div className={styles.container}>
+        
+        <img className={styles.logo} src="logo.svg" alt="" />
+        <main className={styles.main}>
+        <h1 className={styles.title}> </h1>
+            
+         <div className={styles.grid}>
+          <div className={styles.card}>
+            <h1 >Welcome to Systempackage</h1>
+             <div className={styles.username}>
+              <input type="text" name="" id="" className={styles.input} placeholder=' Username' />
+             <select name="" id="" className= { styles.name}>
+              <option value="" className={styles.lastpart}>@gmail.com</option>
+              <option value="">@yahoo.com</option>
+             </select>
+             </div>
+             <div className={styles.username}>
+              <input type="text" name="" id="" className={styles.password} placeholder=' Password' />
+             </div>
+             <button onClick={()=>{router.push("/validate")}} className={styles.btn }>Next</button>
+             <a href="" className={styles.fgt}>Forgot Password?</a>
+          </div> 
+
+          <div className={styles.cardHide } >
+            <div className={styles.secondpart}>
+             <img src="/image.svg" alt="" className={styles.img} />
+            <h6>NEW</h6>
+              <h4>Developer handoff improvements</h4>
+              <p>You&apos;ll now see a highlight around Symbols on the Canvas and in the Inspector.</p>
+            </div>
+          </div>
+
+        </div>
+      </main>
+         <div className={styles.signup}>Not member? <a href="">Create Account</a></div>
+
+     
+    </div>
+  )
+}
+
